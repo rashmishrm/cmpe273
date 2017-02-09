25b829ebe40bcfd8fcf39f39f6b03daff91ca11b
rashmishrm <rashmishrm74@gmail.com>
2017-02-09 02:01:40 -0800

    Added Comments

--- 273-Lab-Practice/lab/socket-mon.py
+++ 273-Lab-Practice/lab/socket-mon.py
@@ -2,16 +2,21 @@ import psutil
 from operator import attrgetter
 from collections import namedtuple
 
-
+#This Function prints all tcp connections grouped by pid,status.
 def print_connections():
         """This Function gets tcp connections and prints status, pid by sorting on the basis of connection status.  """
         connection_array = psutil.net_connections('tcp')
-        myconn = namedtuple('sconn', ['fd', 'family', 'type', 'laddr', 'raddr',
+        #Defining socket_conn as namedTuple
+        socket_conn = namedtuple('sconn', ['fd', 'family', 'type', 'laddr', 'raddr',
                              'status', 'pid'])
+        #Sorting connections on the basis of PID,Status
         sorted_connections = sorted(connection_array, key=attrgetter('pid','status'))
 
+        print '"pid","laddr","raddr","status"'
+
         for x in sorted_connections:
-                p = myconn(*x)
+                #Converting each tuple from list to socket_conn namedTuple for better accessibilty
+                p = socket_conn(*x)
                 if(len(p.laddr)!=0 and len(p.raddr)!=0):
                     laddr = "%s@%s" %(p.laddr[0],p.laddr[1]);
                     raddr = "%s@%s" %(p.raddr[0],p.raddr[1]);
