07e12c7cd2e4770d3014d2fa0fa194ac1501a7f7
rashmishrm <rashmishrm74@gmail.com>
2017-02-15 12:42:18 -0800

    Changed sorting logic

--- 273-Lab-Practice/lab/socket-mon.py
+++ 273-Lab-Practice/lab/socket-mon.py
@@ -1,6 +1,9 @@
 import psutil
+import operator
+
 from operator import attrgetter
 from collections import namedtuple
+from itertools import groupby
 
 #This Function prints all tcp connections grouped by pid,status.
 def print_connections():
@@ -10,17 +13,27 @@ def print_connections():
         socket_conn = namedtuple('sconn', ['fd', 'family', 'type', 'laddr', 'raddr',
                              'status', 'pid'])
         #Sorting connections on the basis of PID,Status
-        sorted_connections = sorted(connection_array, key=attrgetter('pid','status'))
+        sorted_connections = sorted(connection_array, key=attrgetter('pid'))
+        grouped_map = {}
+        pid_count_map={}
+        for key, group in groupby(sorted_connections, key=attrgetter('pid')):
+            grouped_map[key] = [e for e in group]
+            pid_count_map[key]=len(grouped_map[key])
+
+        #sorting pid's by counts in descending order
+        sorted_by_count = sorted(pid_count_map, key=pid_count_map.get, reverse=True)
 
         print '"pid","laddr","raddr","status"'
 
-        for x in sorted_connections:
-                #Converting each tuple from list to socket_conn namedTuple for better accessibilty
-                p = socket_conn(*x)
-                if(len(p.laddr)!=0 and len(p.raddr)!=0):
-                    laddr = "%s@%s" %(p.laddr[0],p.laddr[1]);
-                    raddr = "%s@%s" %(p.raddr[0],p.raddr[1]);
-                    print    '"%s","%s","%s","%s"' %(p.pid,laddr,raddr,p.status)
+        for pid in sorted_by_count:
+            connections_per_pid = grouped_map[pid]
+            for x in connections_per_pid:
+                    #Converting each tuple from list to socket_conn namedTuple for better accessibilty
+                    p = socket_conn(*x)
+                    if(len(p.laddr)!=0 and len(p.raddr)!=0):
+                        laddr = "%s@%s" %(p.laddr[0],p.laddr[1]);
+                        raddr = "%s@%s" %(p.raddr[0],p.raddr[1]);
+                        print    '"%s","%s","%s","%s"' %(p.pid,laddr,raddr,p.status)
 
 
 print_connections()
